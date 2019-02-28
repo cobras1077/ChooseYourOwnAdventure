bfa2bed1db3b67606195a8a5fadace682636a5bd
Martin Wong <martin.wong@amethodschools.org>
2019-02-28 01:53:10 +0000

    updated object choices

--- js/game.js
+++ js/game.js
@@ -6,24 +6,38 @@ var game = {
     levels: {
 
         start: {
-            message: "You come across a dark cave",
+            message: "You are walking down the street and you have come across a man with a knife. He lunges forward toward someone.",
             choices: [
                 {
-                    text: "Enter the cave",
-                    nextLevel: "cave",
+                    text: "Take the hit and save the person",
+                    nextLevel: "revival",
                 },
 
                 {
-                    text: "Keep on moving",
-                    nextLevel: "field",
+                    text: "Move out the way and stand there",
+                    nextLevel: "wait",
                 },
             ]
         },
 
-        cave: {
+        revival: {
             background_image: "fire.gif",
             music: "Final-Fantasy-7-Boss-Battle.mp3",
-            message: "You come across a fire monster or something!",
+            message: "You have died but you have been reincarnated as a slime in another world. On your exploration of the new world you come across a dragon.",
+            choices: [
+                {
+                    text: "Talk to the dragon",
+                    nextLevel: "Confrontation",
+                },
+                {
+                    text: "Ignore and run away fromt the dragon.",
+                    nextLevel: "Lost",
+                }
+            ]
+        },
+
+        wait: {
+            message: "You just witness someone get hurt. You see a pond of blood slowly oozing out of the satvved victim",
             choices: [
                 {
                     text: "Start over",
@@ -31,16 +45,47 @@ var game = {
                 },
             ]
         },
-
-        field: {
-            message: "Some adventurer you are...",
+        
+        Lost: {
+            message:"You have just ran away from your only source of knowledge in this world. You are stranded in the cave.",
             choices: [
                 {
-                    text: "Start over",
-                    nextLevel: "start",
+                    text:"start over",
+                    nextLevel:"start",
                 },
             ]
         },
-
+        
+         Confrontation: {
+            background_image: "fire.gif",
+            music: "Final-Fantasy-7-Boss-Battle.mp3",
+            message: "You learn more about the world and you have befriended this dragon. You hear that this dragon is trapped in the cave because of a spell.",
+            choices: [
+                {
+                    text: "Help him break the spell.",
+                    nextLevel: "Savior",
+                },
+                {
+                    text: "Ignore him and run away.",
+                    nextLevel: "Running",
+                }
+            ]
+        },
+         
+         Savior: {
+            background_image: "fire.gif",
+            music: "Final-Fantasy-7-Boss-Battle.mp3",
+            message: "You decided to use your special skill and engulf the dragon into you. You leave the cave and encountered goblins being threatened by direwolves",
+            choices: [
+                {
+                    text: "Save the goblins",
+                    nextLevel: "goblins",
+                },
+                {
+                    text: "Ignore the goblins because they disgust you",
+                    nextLevel: "wolves",
+                }
+            ]
+        },
     }
 };
